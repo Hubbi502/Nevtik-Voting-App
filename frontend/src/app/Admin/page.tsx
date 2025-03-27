6039aa9fb87b1bf2bae75124db92e6befb523ded
Giovanni Achmad Avicena <63635659+EnderArcherBoy@users.noreply.github.com>
2025-03-27 15:19:35 +0700

    Delete frontend/src/app/Admin/page.tsx

--- frontend/src/app/Admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdminFinal from "../../components/component/views/Admin/Admin-Table/page";
-
-export default function Admin() {
-    return (
-        <div className=" bg-[#F8F2DE]">
-            <AdminFinal />
-        </div>
-    )
-}
